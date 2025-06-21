f2b16ee8e0c225fc56114208efd4120c1a59ec9e
Ragnar <rodiondenmark@gmail.com>
2025-06-21 14:03:17 +0200

    Update dex_info.sql

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -221,4 +221,5 @@ FROM (VALUES
     , ('tapio', 'Tapio', 'Direct', 'Tapio')
     , ('rooster_protocol', 'Rooster Protocol', 'Direct', 'roosterprotocol')
     , ('mindgames', 'Mindgames', 'Direct', 'mindgames_io')
+    , ('mantisswap', 'MantisSwap', 'Direct', 'mantissafinance')
 ) AS temp_table (project, name, marketplace_type, x_username)
